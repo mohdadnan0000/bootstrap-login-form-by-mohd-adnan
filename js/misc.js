f0399f631c6f5f6f6017eddd481ddf6d2d878efa
Mohd Adnan Ansari <31682322+mohdadnan0000@users.noreply.github.com>
2020-03-24 17:37:21 +0530

    Add files via upload

---
+++ js/misc.js
@@ -0,0 +1,27 @@
+//Script For Vertically Centered An Element
+var h = window.innerHeight;
+var h1=(h/2)-200;
+$(document).ready(function(){
+    $('.login-form').css(
+        
+        {"margin-top":""+h1+"px"},
+        
+    );
+});
+
+//Scripts For Toggling Of Password
+$(document).ready(function(){
+	$('.toggle-password').on('click',function(){
+		var x = document.getElementById("password");
+		if (x.type == "password") 
+		{
+			x.type = "text";
+			$(this).html('<span class="fas fa-eye field-icon toggle-password" id="pw-toggle"></span>');
+		}
+		else 
+		{
+			x.type = "password";
+			$(this).html('<span class="fas fa-eye-slash field-icon toggle-password" id="pw-toggle"></span>');
+		}
+	});
+});
